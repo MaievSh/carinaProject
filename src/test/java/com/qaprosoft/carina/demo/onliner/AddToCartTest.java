46a31ab5f19fbdbd5d8d6d4c0a79fdaf6e96082d
Aliaksandra Antsyferava <aantsyferava@Aliaksandras-MacBook-Pro.local>
2021-09-21 14:34:13 +0300

    some fixes

--- src/test/java/com/qaprosoft/carina/demo/onliner/AddToCartTest.java
+++ src/test/java/com/qaprosoft/carina/demo/onliner/AddToCartTest.java
@@ -27,10 +27,11 @@ public class AddToCartTest implements IAbstractTest {
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
         homePage.searchItem("Iphone 12");
         homePage.getIframe();
-        pause(5);
         homePage.chooseItemInCatalog();
         CatalogPageOnliner catalogPage = new CatalogPageOnliner(getDriver());
+        pause(5);
         catalogPage.clickYeas();
+        pause(5);
         homePage.inCart();
         Assert.assertEquals(getDriver().getTitle(),"Корзина заказов onliner.by","Cart page is not opened!");
     }
